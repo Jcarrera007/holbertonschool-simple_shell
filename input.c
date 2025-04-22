77bda29294546a64774e81d766d6f65121392c43
Jcarrera007 <carrerasproductions@gmail.com>
2025-04-22 14:37:30 -0400

    ..

--- input.c
+++ input.c
@@ -1,4 +1,3 @@
-#include "simpleshell.h"
 
 /**
  * read_line - reads a line of input from stdin
@@ -7,24 +6,24 @@
  */
 char *read_line(void)
 {
-    char   *line = NULL;
-    size_t bufsize = 0;
-    ssize_t n;
+	char   *line = NULL;
+	size_t bufsize = 0;
+	ssize_t n;
 
-    n = getline(&line, &bufsize, stdin);
-    if (n == -1)
-    {
-        if (feof(stdin))
-        {
-            printf("\n");
-            exit(EXIT_SUCCESS);
-        }
-        else
-        {
-            perror("getline");
-            exit(EXIT_FAILURE);
-        }
-    }
+	n = getline(&line, &bufsize, stdin);
+	if (n == -1)
+	{
+		if (feof(stdin))
+		{
+			printf("\n");
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			perror("getline");
+			exit(EXIT_FAILURE);
+		}
+	}
 
-    return (line);
+	return (line);
 }
